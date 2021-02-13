b4b99244f02e30c3bcab0f57981b7e1573b7613a
Loren Rachelle Hicks <lorenhicks@gmail.com>
2021-02-13 09:13:54 -0500

    Refactoring Code

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
-const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-const session = require("express-session");
-const FileStore = require("session-file-store")(session);
 const passport = require("passport");
-const authenticate = require("./authenticate");
+const config = require("./config");
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
@@ -18,7 +15,6 @@ const mongoose = require("mongoose");
 
 const config = require("./config");
 
-const url = "mongodb://localhost:27017/nucampsite";
 const url = config.mongoUrl;
 const connect = mongoose.connect(url, {
   useCreateIndex: true,
@@ -43,21 +39,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 //app.use(cookieParser("12345-67890-12345-67890"));
 
-app.use(
-  session({
-    name: "session-id",
-    secret: "12345-67890-12345-67890",
-    saveUninitialized: false,
-    resave: false,
-    store: new FileStore(),
-  })
-);
-
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
 app.use(passport.initialize());
-app.use(passport.session());
 
 app.use(express.static(path.join(__dirname, "public")));
 
