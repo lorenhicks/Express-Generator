96e9eaf032b05f9ee3be09f77fdbfac3fb0d8205
Loren Rachelle Hicks <lorenhicks@gmail.com>
2021-02-20 01:09:54 -0500

    Passport Facebook Init

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   secretKey: "12345-67890-09876-54321",
   mongoUrl: "mongodb://localhost:27017/nucampsite",
+  facebook: {
+    clientId: "412790746489850",
+    clientSecret: "f724d003a75e3014c0b6209289b6d9e6",
+  },
 };
